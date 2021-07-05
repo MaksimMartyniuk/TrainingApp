d222bf25341943200af90c97866d0485c2de1e71
MaksimMartyniuk <Maksim_Martyniuk@epam.com>
2021-07-05 13:07:47 +0300

    Adding repos

---
+++ TrainingApp/Repositories/MSSQL/MSSQLContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TrainingApp.BusinessObjects;
+
+namespace TrainingApp.Repositories.MSSQL
+{
+	public class MSSQLContext : DbContext
+	{
+		public MSSQLContext()
+		{
+			this.Database.EnsureCreated();
+		}
+		public DbSet<User> Users { get; set; }
+		public DbSet<Passport> Passports { get; set; }
+		public DbSet<Order> Orders { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			IConfigurationRoot configuration = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultSQLConnection"));
+		}
+	}
+}
---
+++ TrainingApp/Repositories/MSSQL/UserEntityMSSQLRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using TrainingApp.BusinessObjects;
+
+namespace TrainingApp.Repositories.MSSQL
+{
+	public class UserEntityMSSQLRepository : IRepository<User>
+	{
+		public void Create(User item)
+		{
+			using (MSSQLContext context = new MSSQLContext())
+			{
+				context.Users.Add(item);
+				context.SaveChanges();
+			}
+		}
+		public User GetObject(Guid id)
+		{
+			using (MSSQLContext context = new MSSQLContext())
+			{
+				return context.Users.Find(id);
+			}
+		}
+
+		public void Update(User item)
+		{
+			using (MSSQLContext context = new MSSQLContext())
+			{
+				context.Entry(item).State = EntityState.Modified;
+				context.SaveChanges();
+			}
+		}
+
+		public void Delete(Guid id)
+		{
+			using (MSSQLContext context = new MSSQLContext())
+			{
+				User user = context.Users.Find(id);
+				if (user != null)
+				{
+					context.Users.Remove(user);
+					context.SaveChanges();
+				}
+			}
+		}
+
+		public IEnumerable<User> GetObjectList()
+		{
+			using (MSSQLContext context = new MSSQLContext())
+			{
+				return context.Users;
+			}
+		}
+	}
+}
